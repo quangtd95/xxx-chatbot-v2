e5a5c118790cd13ed01a81f036e406711b755dc5
Thai Duy Quang (FHO.CTC) <QuangTD7@fsoft.com.vn>
2017-07-05 10:18:24 +0700

    as

--- util/service-utils.js
+++ util/service-utils.js
@@ -1,10 +1,17 @@
 var object = require('../model/object.js');
 var api_util = require('../util/apiai-utils.js');
+var db = require('../lib/db/dbHelper.js');
 module.exports = {
 	handle : function (action,req,res) {
 		switch (action) {
 			case 'service.user.want.send.money':
 			chooseServiceSendMoney(req,res);
+			break;
+			case 'service.user.ask.balance':
+			askBalance(req,res);
+			break;
+			case 'service.user.ask.history':
+			askHistory(req,res);
 			break;
 		}		
 	}
@@ -18,4 +25,34 @@ function chooseServiceSendMoney(request,response){
 	api_util.addContext(contexts,'ask_name_to_send',3,{})
 	api_util.addContext(contexts,'send_money',100,{});
 	return response.json(api_util.makeJsonResponse(reply,source,contexts));
+},
+
+function askBalance(request,response){
+	var reply;
+	var contexts = [];
+	var source;
+	var curAccount = api_util.getParamsOfContext(request,'authentication_pass').account_number;
+	db.checkBalance(curAccount,function(success,data){
+		if (!success){
+			reply = "sorry, server is unavailable now. please try again later!";
+		} else {
+			reply = "You have "+data+" vnd now. What do you want to do next? ";
+		}
+		return response.json(api_util.makeJsonResponse(reply,source,contexts));
+	});
+}
+
+function askHistory(request,response){
+	var reply;
+	var contexts = []
+	var source;
+	var curAccount = api_util.getParamsOfContext(request,'authentication_pass').account_number;
+	db.getTradingHistory(curAccount,function (success,data){
+		if (!success){
+			reply = "sorry, server is unavailable now. please try again later";
+		} else {
+			reply = "Your transaction history: \n"+data;
+		}
+		return response.json(api_util.makeJsonResponse(reply,source,contexts));
+	});
 }
